2520aba6fba4b4757fa472edf704d48ddf05f16f
Matipa Marufu <2344503@students.wits.ac.za>
2024-06-05 23:49:27 +0100

    extended book table

--- app.py
+++ app.py
@@ -5,8 +5,15 @@ st.title("Let's submit some books man!")
 
 title = st.text_area('Enter title:')
 author = st.text_area('Enter Author:')
+publisher = st.text_area('Enter Publisher:')
+year_purchased = st.text_area('Enter Year Published:')
+description = st.text_area('Enter description:')
+secondary_title = st.text_area('Enter secondary title:')
+version = st.text_area('Enter version:')
+
+
 if st.button('Submit'):
-    response = requests.post('http://127.0.0.1:5000/crazy_books', json={'title': title, 'author': author})
+    response = requests.post('http://127.0.0.1:5000/crazy_books', json={'title': title, 'author': author, 'publisher': publisher, 'year_purchased': year_purchased, 'description': description, 'secondary_title':secondary_title, version:'version'})
     if response.status_code == 201:
         st.success('Books submitted!')
     else:
--- server.py
+++ server.py
@@ -1,19 +1,8 @@
 from flask import Flask, request, jsonify
-#import mysql.connector
-#from mysql.conncetor import Error
 import sqlite3
 from sqlite3 import Error
 
 app = Flask(__name__)
-
-#def connection():
-#    return mysql.connector.connect(
-#                host='localhost',
-#                user='username',
-#                password='password',
-#                database='database'
-#            )
-
 
 def connection():
     conn = None
@@ -27,19 +16,53 @@ def connection():
 def save():
     title = request.json.get('title')
     author = request.json.get('author')
-    if not title or not author:
+    publisher = request.json.get('publisher')
+    description = request.json.get('description')  
+    secondary_title = request.json.get('secondary_title')
+    version = request.json.get('publisher')
+    year_purchased= request.json.get('year_purchased')  
+
+    if not title or not author or not publisher or not description or not secondary_title or not version or not year_purchased:
         return jsonify({'error': 'No data provided'}), 400
-    
+
     try:
         connect = connection()
         cursor = connect.cursor()
-        cursor.execute('INSERT INTO Books (title, author) VALUES (?, ?)', (title, author))
+        cursor.execute('INSERT INTO Books (title, author, publisher, description, secondary_title, version, year_purchased) VALUES (?, ?, ?, ?, ?, ?, ?)', (title, author, publisher, description, secondary_title, version, year_purchased))
         connect.commit()
         cursor.close()
         connect.close()
         return jsonify({'message': 'Data saved'}), 201
-    except:
+    except Error as e:
+        print(e)
         return jsonify({'error': 'Error saving data'}), 500
+    
+
+# USER TABLE
+
+# def save_user():
+#     title = request.json.get('first_name')
+#     author = request.json.get('last_name')
+#     publisher = request.json.get('profile_picture')
+#     description = request.json.get('description')  
+#     secondary_title = request.json.get('secondary_title')
+#     version = request.json.get('publisher')
+#     year_purchased= request.json.get('year_purchased')  
+
+#     if not title or not author or not publisher or not description or not secondary_title or not version or not year_purchased:
+#         return jsonify({'error': 'No data provided'}), 400
+
+#     try:
+#         connect = connection()
+#         cursor = connect.cursor()
+#         cursor.execute('INSERT INTO Books (title, author, publisher, description, secondary_title, version, year_purchased) VALUES (?, ?, ?, ?, ?, ?, ?)', (title, author, publisher, description, secondary_title, version, year_purchased))
+#         connect.commit()
+#         cursor.close()
+#         connect.close()
+#         return jsonify({'message': 'Data saved'}), 201
+#     except Error as e:
+#         print(e)
+#         return jsonify({'error': 'Error saving data'}), 500
     
 if __name__ == '__main__':
     app.run(port=5000)
