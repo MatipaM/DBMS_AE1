3011bfe8cef3a42a948e7a10fcc35bff0d3d2aad
Matipa Marufu <2344503@students.wits.ac.za>
2024-06-06 00:29:24 +0100

    added registration page
    still has bugs

--- Registration.py
+++ Registration.py
@@ -1,5 +1,21 @@
 import streamlit as st
+import requests
 
-st.header("Registration Page")
-st.write("")
-st.button("Register Here", on_click=None,type="secondary", disabled=False, use_container_width=False)
+
+st.title("Registration Page")
+
+first_name = st.text_input('Enter First Name:')
+last_name = st.text_input('Enter Last Name:')
+profile_picture = st.text_area('Upload profile picture:')
+address = st.text_area('Enter address:')
+phone = st.text_input('Enter phone number:')
+email = st.text_input('Enter email:')
+password = st.text_input('Create password:')
+
+
+if st.button('Submit'):
+    response = requests.post('http://127.0.0.1:5000/crazy_user', json={'first_name': first_name, 'last_name': last_name, 'address': address, 'phone': phone, 'email': email, 'password':password, 'profile_picture': profile_picture})
+    if response.status_code == 201:
+        st.success('Account Created!')
+    else:
+        st.error('Failed to create User account')
--- server.py
+++ server.py
@@ -40,29 +40,30 @@ def save():
 
 # USER TABLE
 
-# def save_user():
-#     title = request.json.get('first_name')
-#     author = request.json.get('last_name')
-#     publisher = request.json.get('profile_picture')
-#     description = request.json.get('description')  
-#     secondary_title = request.json.get('secondary_title')
-#     version = request.json.get('publisher')
-#     year_purchased= request.json.get('year_purchased')  
+@app.route('/crazy_user', methods=['POST'])
+def save_user():
+    first_name = request.json.get('first_name')
+    last_name = request.json.get('last_name')
+    profile_picture = request.json.get('profile_picture')
+    address = request.json.get('address')  
+    phone = request.json.get('phone')
+    email = request.json.get('email')
+    password= request.json.get('password')  
 
-#     if not title or not author or not publisher or not description or not secondary_title or not version or not year_purchased:
-#         return jsonify({'error': 'No data provided'}), 400
+    if not first_name or not last_name or not profile_picture or not address or not phone or not email or not password:
+        return jsonify({'error': 'No data provided'}), 400
 
-#     try:
-#         connect = connection()
-#         cursor = connect.cursor()
-#         cursor.execute('INSERT INTO Books (title, author, publisher, description, secondary_title, version, year_purchased) VALUES (?, ?, ?, ?, ?, ?, ?)', (title, author, publisher, description, secondary_title, version, year_purchased))
-#         connect.commit()
-#         cursor.close()
-#         connect.close()
-#         return jsonify({'message': 'Data saved'}), 201
-#     except Error as e:
-#         print(e)
-#         return jsonify({'error': 'Error saving data'}), 500
+    try:
+        connect = connection()
+        cursor = connect.cursor()
+        cursor.execute('INSERT INTO USER (first_name, last_name, profile_picture, address, phone, email, password) VALUES (?, ?, ?, ?, ?, ?, ?)', (first_name, last_name, profile_picture, address, phone, email, password))
+        connect.commit()
+        cursor.close()
+        connect.close()
+        return jsonify({'message': 'Data saved'}), 201
+    except Error as e:
+        print(e)
+        return jsonify({'error': 'Error saving data'}), 500
     
 if __name__ == '__main__':
     app.run(port=5000)
