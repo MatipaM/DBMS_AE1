93ac65517cb789d8cc167221f53242407f6e8342
kitchan98 <81081193+kitchan98@users.noreply.github.com>
2024-06-06 22:50:35 +0100

    librarian review part 1

--- pages/borrow.py
+++ pages/borrow.py
@@ -5,12 +5,16 @@ from datetime import datetime
 
 st.title("Let's Borrow Book")
 
-user_id = st.text_input('Enter User ID:')
-book_title = st.text_input('Enter Book Title:')
+email = st.text_input('Email:')
+book_title = st.text_input('Book Title:')
+affiliation = st.text_input('Affiliation:')
+interest = st.text_input('Book Interest:')
 date_borrowed = st.date_input('Date Borrowed', datetime.today())
 
 if st.button('Submit'):
-    response = requests.post('http://127.0.0.1:5000/crazy_borrow', json={'user_id': user_id, 'book_title': book_title, 'date_borrowed': date_borrowed})
+    date_borrowed = date_borrowed.strftime('%Y-%m-%d')
+    response = requests.post('http://127.0.0.1:5000/crazy_borrow', json={'eamil': email, 'book_title': book_title, 'date_borrowed': date_borrowed, 'affiliation': affiliation, 'interest': interest})
+    st.switch_page("pages/wait.py")
     if response.status_code == 201:
         st.success('Books submitted!')
     else:
---
+++ pages/wait.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.title("Your borrow request is being processed, please wait for a confirmation from the Librarian. Thank you!")
