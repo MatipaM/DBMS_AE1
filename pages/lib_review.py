8aaa61e2fa0c03be2d831372c889780349c25a2c
kitchan98 <81081193+kitchan98@users.noreply.github.com>
2024-06-07 17:12:24 +0100

    lib_review criteria done

--- pages/lib_review.py
+++ pages/lib_review.py
@@ -11,7 +11,6 @@ cursor = conn.cursor()
 cursor.execute("SELECT * FROM Pending_Request")
 rows = cursor.fetchall()
 columns = [description[0] for description in cursor.description]
-conn.close()
 
 st.write("### Pending Book Requests")
 
@@ -20,3 +19,59 @@ if rows:
     df = st.data_editor(df, num_rows="dynamic")
 else:
     st.write("No Book Requests.")
+
+
+# Criteria 1 (Returned all books)
+
+st.write("### Checklist 1: Outstanding Books")
+cursor.execute("SELECT br.Title, br.Borrowed_Date, br.Returned_Date, br.Email AS Borrower_Email, br.Rating, br.Review, pr.Affiliation, pr.Interest, pr.Email AS Requester_Email, pr.Request_Date FROM Book_Records br JOIN Pending_Request pr ON br.Email = pr.Email WHERE (br.Borrowed_Date IS NOT NULL AND br.Returned_Date IS NOT NULL) OR (br.Borrowed_Date IS NOT NULL AND br.Returned_Date IS NULL)")
+row2 = cursor.fetchall()
+column2 = [description[0] for description in cursor.description]
+
+if row2:
+    df2 = pd.DataFrame(row2, columns=column2)
+    df2 = st.data_editor(df2, num_rows="dynamic")
+else:
+    st.write("No outstanding books.")
+
+# Criteria 2 (Paid outstanding bills)
+
+st.write("### Checklist 2: Outstanding Bills")
+cursor.execute("SELECT ob.Email AS Outstanding_Bill_Email, ob.Amount FROM Outstanding_Bills ob JOIN Pending_Request pr ON ob.Email = pr.Email WHERE ob.Amount IS NOT NULL")
+row3 = cursor.fetchall()
+column3 = [description[0] for description in cursor.description]
+
+if row3:
+    df3 = pd.DataFrame(row3, columns=column3)
+    df3 = st.data_editor(df3, num_rows="dynamic")
+else:
+    st.write("No outstanding bills.")
+
+# Criteria 3 (Current Student)
+
+st.write("### Checklist 3: Current Student")
+
+cursor.execute("SELECT Email, Affiliation FROM Pending_Request")
+row4 = cursor.fetchall()
+column4 = [description[0] for description in cursor.description]
+conn.close()
+
+if row4:
+    df4 = pd.DataFrame(row4, columns=column4)
+    df4 = st.data_editor(df4, num_rows="dynamic")
+else:
+    st.write("No current students.")
+
+# Approve or Disapprove Button
+
+if st.button("Approve"):
+    st.switch_page("pages/wait.py")
+    #Approved Database need to be updated
+    #.......
+
+elif st.button("Disapprove"):
+    st.switch_page("pages/wait.py")
+    #Students need to be notified
+    #......
+
+
